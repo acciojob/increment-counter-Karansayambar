fd680f98779607fe1486f8f8294e11ca5f98fd64
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-29 08:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const btn = document.getElementById("incrementBtn");
+const para = document.getElementById("counter");
+
+let count = 0;
+btn.addEventListener('click' , ()=>{
+	para.innerText = count++;
+})
